286604be8f9e89e0151ecd6d5ac10d76fed9764a
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2021-01-28 14:48:59 +0100

    add token

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo('Hello world !');
+require('vendor/autoload.php');
+
+// $_SERVER['REQUEST_URI']  
+
+// Si mon url != "api-slack-token-nlc.herokuapp.com/"
+
+if($_SERVER['HTTP_HOST'] !=  "api-slack-token-nlc.herokuapp.com") {
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+  $dotenv->load();
+}
+
+
+
+// Sinon je ne le fais pas
+
+$ary = array("token" => $_ENV['TOKEN'], "uri" => $_SERVER['HTTP_HOST']);
+$data = json_encode($ary);
+
+echo($data);
